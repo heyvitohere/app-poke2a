f7f60d73346d3eab0ffd332af5c306255f70345f
heyvitohere <victorsoa9@gmail.com>
2024-05-06 08:28:08 -0300

    pokeapi

---
+++ src/listagem/lista_pokemon.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect}from 'react'
+
+const Listagem = () => (
+    <div>
+        <ul>
+            <li>Pokémon 1</li>
+            <li>Pokémon 2</li>
+            <li>Pokémon 3</li>
+            <li>Pokémon 4</li>
+            <li>Pokémon 5</li>
+        </ul>
+    </div>
+);
+
+export default Listagem 
